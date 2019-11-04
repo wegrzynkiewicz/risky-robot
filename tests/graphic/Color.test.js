bfcddb2636ec702e5b9e348d0348b0075afc035a
Łukasz Węgrzynkiewicz <lukasz.wegrzynkiewicz@givt.com>
2019-11-04 13:12:42 +0100

    Added Color class

---
+++ tests/graphic/Color.test.js
@@ -0,0 +1,7 @@
+import Color from "../../lib/graphic/Color";
+
+describe("Color", function () {
+    it("should created successful", function () {
+        const color = new Color(0, 0, 0, 0);
+    });
+});
