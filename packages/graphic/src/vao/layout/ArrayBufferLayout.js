e5ff39848573f3cf8e3aa37cf9163c9addd45149
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2020-01-16 22:40:25 +0100

    Development

--- packages/graphic/src/vao/layout/ArrayBufferLayout.js
+++ packages/graphic/src/vao/layout/ArrayBufferLayout.js
@@ -5,6 +5,15 @@ export default class ArrayBufferLayout {
         this.attributeBatchLayoutSet = new Set();
     }
 
+    getAttributeLayoutByName(attributeLayoutName) {
+        for (const {attributeLayoutMap} of this.attributeBatchLayoutSet.values()) {
+            if (attributeLayoutMap.has(attributeLayoutName)) {
+                return attributeLayoutMap.get(attributeLayoutName);
+            }
+        }
+        throw new Error(`Not found AttributeLayout named (${attributeLayoutName})`);
+    }
+
     createArrayBufferByDataView() {
         const buffer = new ArrayBuffer(this.bufferLength);
         const dataView = new DataView(buffer);
--- packages/graphic/tests/unit/vao/blueprint/ArrayBufferBlueprint.test.js
+++ packages/graphic/tests/unit/vao/blueprint/ArrayBufferBlueprint.test.js
@@ -2,8 +2,23 @@ import assert from "assert";
 import Allocation from "../../../../src/vao/layout/Allocation";
 import VertexLayoutBlueprint from "../../../../src/vao/blueprint/VertexLayoutBlueprint";
 
+function assertAttributeLayout(bufferLayout, attributeName, expectedStride, expectedOffset) {
+    const {stride, offset} = bufferLayout.getAttributeLayoutByName(attributeName);
+    assert.strictEqual(
+        stride,
+        expectedStride,
+        `Invalid stride attribute named (${attributeName})`
+    );
+    assert.strictEqual(
+        offset,
+        expectedOffset,
+        `Invalid offset attribute named (${attributeName})`
+    );
+}
+
 describe("ArrayBufferBlueprint", function () {
-    it("should create valid ArrayBufferAllocation object", function () {
+
+    it("should create valid ArrayBufferLayout object", function () {
         const bufferBlueprint = new VertexLayoutBlueprint.ArrayBuffer({
             batches: [
                 new VertexLayoutBlueprint.AttributeBatch({
@@ -48,5 +63,111 @@ describe("ArrayBufferBlueprint", function () {
         const verticesCount = 6;
         const expectedLength = v => (v * 12) + (v * 8) + (v / 3 * 12) + (v / 6 * 16);
         assert.strictEqual(bufferLayout.byteLength, expectedLength(verticesCount));
+    });
+
+    it("should create valid complex ArrayBufferLayout object", function () {
+        const bufferBlueprint = new VertexLayoutBlueprint.ArrayBuffer({
+            type: "array",
+            schema: "abc/de/f/g/hijk",
+            batches: [
+                new VertexLayoutBlueprint.AttributeBatch({
+                    attributes: [
+                        new VertexLayoutBlueprint.Attribute({
+                            name: "a_VertexPosition",
+                            type: "vec3<f32>"
+                        }),
+                        new VertexLayoutBlueprint.Attribute({
+                            name: "a_VertexNormal",
+                            type: "vec3<f32>"
+                        }),
+                        new VertexLayoutBlueprint.Attribute({
+                            name: "a_VertexTexCoords",
+                            type: "vec2<f32>"
+                        }),
+                    ],
+                }),
+                new VertexLayoutBlueprint.AttributeBatch({
+                    attributes: [
+                        new VertexLayoutBlueprint.Attribute({
+                            name: "a_VertexColor",
+                            type: "vec4<f32>"
+                        }),
+                        new VertexLayoutBlueprint.Attribute({
+                            name: "a_VertexNegativeColor",
+                            type: "f32"
+                        }),
+                    ],
+                }),
+                new VertexLayoutBlueprint.AttributeBatch({
+                    attributes: [
+                        new VertexLayoutBlueprint.Attribute({
+                            name: "a_VertexWeight",
+                            type: "s16"
+                        }),
+                    ],
+                }),
+                new VertexLayoutBlueprint.AttributeBatch({
+                    attributes: [
+                        new VertexLayoutBlueprint.Attribute({
+                            name: "a_VertexVisibility",
+                            type: "s8"
+                        }),
+                    ],
+                }),
+                new VertexLayoutBlueprint.AttributeBatch({
+                    attributes: [
+                        new VertexLayoutBlueprint.Attribute({
+                            name: "a_VertexAdditional1",
+                            type: "u16"
+                        }),
+                        new VertexLayoutBlueprint.Attribute({
+                            name: "a_VertexAdditional2",
+                            type: "u16"
+                        }),
+                        new VertexLayoutBlueprint.Attribute({
+                            name: "a_VertexAdditional3",
+                            type: "u32"
+                        }),
+                        new VertexLayoutBlueprint.Attribute({
+                            name: "a_VertexAdditional4",
+                            type: "vec4<s16>"
+                        }),
+                    ],
+                }),
+
+            ],
+        });
+
+        const allocation = new Allocation({
+            primitive: "triangle",
+            elements: 10,
+        });
+
+        const bufferLayout = bufferBlueprint.createBufferLayout(allocation);
+        const verticesCount = 30;
+
+        assertAttributeLayout(bufferLayout, "a_VertexPosition", 32, 0);
+        assertAttributeLayout(bufferLayout, "a_VertexNormal", 32, 12);
+        assertAttributeLayout(bufferLayout, "a_VertexTexCoords", 32, 24);
+
+        const block1Offset = verticesCount * 32;
+        assertAttributeLayout(bufferLayout, "a_VertexColor", 20, block1Offset);
+        assertAttributeLayout(bufferLayout, "a_VertexNegativeColor", 20, block1Offset + 16);
+
+        const block2Offset = block1Offset + (verticesCount * 20);
+        assertAttributeLayout(bufferLayout, "a_VertexWeight", 2, block2Offset);
+
+        const block3Offset = block2Offset + (verticesCount * 2);
+        assertAttributeLayout(bufferLayout, "a_VertexVisibility", 1, block3Offset);
+
+        const block4Offset = block3Offset + verticesCount;
+        assertAttributeLayout(bufferLayout, "a_VertexAdditional1", 16, block4Offset);
+        assertAttributeLayout(bufferLayout, "a_VertexAdditional2", 16, block4Offset + 2);
+        assertAttributeLayout(bufferLayout, "a_VertexAdditional3", 16, block4Offset + 4);
+        assertAttributeLayout(bufferLayout, "a_VertexAdditional4", 16, block4Offset + 8);
+
+        const vertexByteLength = 71;
+        const byteLength = verticesCount * vertexByteLength;
+        assert.strictEqual(bufferLayout.byteLength, byteLength);
     });
 });
