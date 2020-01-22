1b03349a0d9f86b00bec03995123f38784509d7e
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2020-01-22 22:10:49 +0100

    Development

--- packages/game-client/src/objects/wall/WallSceneNode.js
+++ packages/game-client/src/objects/wall/WallSceneNode.js
@@ -3,12 +3,22 @@ import * as glMatrix from "gl-matrix";
 import WallVAOGenerator from "../../wall/WallVAOGenerator";
 import VAOCreator from "../../layout/VAOCreator";
 
+const set = function (dataview, offset, array, number) {
+    for (let i = 0; i < number; i++) {
+        const o = offset + (i * 4);
+        dataview.setFloat32(o, array[i], true);
+    }
+};
+
 export default class WallSceneNode extends SceneNode {
 
     constructor(game) {
         super({});
 
         const {openGL} = game;
+
+        this.modelMatrix = glMatrix.mat4.create();
+        this.modelMatrix = glMatrix.mat4.translate(this.modelMatrix, this.modelMatrix, [-8.0, 8.0, -5.0]);
 
         this.shader = game.shaderManager.getShaderByName("wall");
 
@@ -28,10 +38,37 @@ export default class WallSceneNode extends SceneNode {
             },
         });
 
-        console.log(dataView);
+        const gl = openGL;
+        const program = this.shader.program;
 
-        this.modelMatrix = glMatrix.mat4.create();
-        this.modelMatrix = glMatrix.mat4.translate(this.modelMatrix, this.modelMatrix, [-8.0, 8.0, -5.0]);
+        const blockIndex_1 = gl.getUniformBlockIndex(program, "TestBlock");
+        console.log('blockIndex_1', blockIndex_1);
+        const blockSize_1 = gl.getActiveUniformBlockParameter(program, blockIndex_1, gl.UNIFORM_BLOCK_DATA_SIZE);
+        console.log('blockSize_1', blockSize_1);
+
+        const uniformIndices_1 = gl.getUniformIndices(program, ["u_projectionMatrix", "u_modelMatrix", "u_viewMatrix1"]);
+        //const uniformIndices_1 = gl.getUniformIndices(program, ["u_modelMatrix1"]);
+        console.log('uniformIndices_1', uniformIndices_1);
+
+        this.uniformOffsets_1 = gl.getActiveUniforms(program, uniformIndices_1, gl.UNIFORM_OFFSET);
+        console.log('uniformOffsets_1', this.uniformOffsets_1);
+
+        const uboArray = new ArrayBuffer(blockSize_1);
+        this.dataview = new DataView(uboArray);
+        const float32 = new Float32Array(uboArray);
+
+        set(this.dataview, this.uniformOffsets_1[0], game.camera.getProjectionMatrix(), 16);
+        set(this.dataview, this.uniformOffsets_1[1], this.modelMatrix, 16);
+
+        console.log(float32);
+
+        const binding = 4;
+        this.b1 = gl.createBuffer();
+        gl.bindBuffer(gl.UNIFORM_BUFFER, this.b1);
+        gl.bufferData(gl.UNIFORM_BUFFER, uboArray, gl.DYNAMIC_DRAW);
+        gl.uniformBlockBinding(program, blockIndex_1, binding);
+        gl.bindBufferBase(gl.UNIFORM_BUFFER, binding, this.b1);
+        gl.bindBuffer(gl.UNIFORM_BUFFER, null);
     }
 
     render(game) {
@@ -40,9 +77,13 @@ export default class WallSceneNode extends SceneNode {
         openGL.bindVertexArray(this.vao.openGLVAOPointer);
         openGL.useProgram(this.shader.program);
 
-        openGL.uniformMatrix4fv(this.shader.uniforms['u_projectionMatrix'], false, game.camera.getProjectionMatrix());
-        openGL.uniformMatrix4fv(this.shader.uniforms['u_viewMatrix'], false, game.camera.getViewMatrix());
-        openGL.uniformMatrix4fv(this.shader.uniforms['u_modelMatrix'], false, this.modelMatrix);
+        // openGL.uniformMatrix4fv(this.shader.uniforms['u_projectionMatrix'], false, game.camera.getProjectionMatrix());
+        // openGL.uniformMatrix4fv(this.shader.uniforms['u_viewMatrix'], false, game.camera.getViewMatrix());
+        // openGL.uniformMatrix4fv(this.shader.uniforms['u_modelMatrix'], false, this.modelMatrix);
+
+        set(this.dataview, this.uniformOffsets_1[2], game.camera.getViewMatrix(), 16);
+        openGL.bindBuffer(openGL.UNIFORM_BUFFER, this.b1);
+        openGL.bufferSubData(openGL.UNIFORM_BUFFER, 0, this.dataview, 0, 16*4);
 
         const vertices = this.wallVAOGenerator.vertices;
         const {glDrawType} = this.vao.vaoAllocation;
--- packages/game-client/src/objects/wall/shader/wall.vert
+++ packages/game-client/src/objects/wall/shader/wall.vert
@@ -3,9 +3,12 @@
 layout(location=0) in vec3 a_VertexPosition;
 layout(location=1) in vec3 a_VertexNormal;
 
-uniform highp mat4 u_modelMatrix;
 uniform highp mat4 u_viewMatrix;
-uniform highp mat4 u_projectionMatrix;
+uniform TestBlock {
+    highp mat4 u_viewMatrix1;
+    highp mat4 u_modelMatrix;
+    highp mat4 u_projectionMatrix;
+};
 
 out vec3 v_fragmentPosition;
 out vec3 v_fragmentNormal;
@@ -13,5 +16,5 @@ out vec3 v_fragmentNormal;
 void main(void) {
     v_fragmentPosition = vec3(u_modelMatrix * vec4(a_VertexPosition, 1.0));
     v_fragmentNormal = a_VertexNormal;
-    gl_Position = u_projectionMatrix * u_viewMatrix * vec4(v_fragmentPosition, 1.0);
+    gl_Position = u_projectionMatrix * u_viewMatrix1 * vec4(v_fragmentPosition, 1.0);
 }
