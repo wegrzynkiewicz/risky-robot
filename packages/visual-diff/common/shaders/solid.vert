55370b384e4c3c5a15c6da522bc84d57ee5dc07f
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2020-02-16 18:40:22 +0100

    Development

--- packages/visual-diff/common/shaders/solid.vert
+++ packages/visual-diff/common/shaders/solid.vert
@@ -3,5 +3,5 @@
 layout(location = 0) in vec3 a_Position;
 
 void main(void) {
-    gl_Position = vec4(a_Position, 1.0);
+    gl_Position = vec4(a_Position - 0.5, 1.0);
 }
---
+++ packages/visual-diff/dataset/1051-gltf-simple-meshes/entry.js
@@ -0,0 +1,28 @@
+import * as Frontend from "robo24-frontend";
+import vertexShaderContent from "../../common/shaders/solid.vert";
+import fragmentShaderContent from "../../common/shaders/solid.frag";
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const canvas = document.getElementById("canvas");
+    const config = JSON.parse(document.getElementById("config").innerHTML);
+    const system = Frontend.createBasicSystem({window, canvas});
+
+    const format = config["suiteName"].indexOf("binary") === -1 ? "gltf" : "glb";
+    const resource = new Frontend.Assets.Resource({
+        vendor: "@base",
+        mimeType: "model/gltf+json",
+        url: `/visual-diff/dataset/${config["suiteName"]}/model.${format}`,
+    });
+
+    const {gltfManager, view} = system;
+    view.programManager.registerShaderContent("solid", vertexShaderContent, fragmentShaderContent);
+
+    const gltfContent = await gltfManager.loadContent(resource);
+    const asset = await gltfManager.extractAsset({view, gltfContent});
+
+    const sceneNode = asset.createScene(0);
+    const primaryScene = system.view.sceneManager.find(n => n.name === "primary-scene");
+    sceneNode.setParent(primaryScene);
+
+    Frontend.Graphic.printSceneNode(system.view.sceneManager);
+});
